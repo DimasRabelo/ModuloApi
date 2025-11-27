ec12c77bc5e655f76537c7e7af9a434a511b2cd7
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-11-27 17:50:10 -0300

    Alterando Edpoint Create

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -28,7 +28,7 @@ namespace ModuloApi.Controllers
         {
             _context.Add(contato);
             _context.SaveChanges();
-            return Ok(contato);
+            return CreatedAtAction(nameof(ObterPorId), new { id = contato.id }, contato);
         }
 
         [HttpGet("{id}")]
@@ -42,6 +42,12 @@ namespace ModuloApi.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
         [HttpPut("id")]
         public  IActionResult Atualizar(int id, Contato contato)
         {
